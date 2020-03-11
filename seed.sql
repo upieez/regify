e4f28209553a0ebb8880cd8e5843eabc0a430922
upieez <samuelhww@gmail.com>
2020-03-11 21:38:32 +0800

    change seed data again to single quotes and add a unique type for companies name;

--- seed.sql
+++ seed.sql
@@ -5,7 +5,7 @@ INSERT INTO companies (name, password) VALUES ('charityorganisation', '123');
 INSERT INTO companies (name, password) VALUES ('besteventcompany', '123');
 INSERT INTO companies (name, password) VALUES ('admin','admin');
 
-INSERT INTO events (company_id,name) VALUES (6, "Graduation D&D 2020");
+INSERT INTO events (company_id,name) VALUES (6, 'Graduation D&D 2020');
 
 INSERT INTO attendees (event_id, attendee_id, name, table_number, has_attended, eligibility) VALUES (1, 8000, 'Malcom', '1', false, true);
 INSERT INTO attendees (event_id, attendee_id, name, table_number, has_attended, eligibility) VALUES (1, 8001, 'Peter', '1', false, true);
--- tables.sql
+++ tables.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS companies (
     id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
+    name TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL
 );
 
